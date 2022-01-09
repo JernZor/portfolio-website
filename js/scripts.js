7164a036c0e2352047e64724cd38ec0547cf5fa5
JernZor <95475586+JernZor@users.noreply.github.com>
2022-01-09 15:11:48 +0100

    Added hamburger menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("hello world")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+  
